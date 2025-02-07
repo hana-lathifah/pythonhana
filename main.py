d1125f5e62b7863619e3cf641d4fb0f67a5dc25e
hana-lathifah <hana.lathifah@student.alhazen.academy>
2025-02-07 11:49:31 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+print("hello world!")
+print("selamat datang")
+
+print("hello",end=" ")
+print("semuanya")
+
+name="budi"
+age= 20
+
+print(name)
+print(age)
+
+print("hello", name,".",age,"years old")
+print("hello"+ name+","+str(age) +"years old")
+print(f"Hello {name}, {age} years old")
+
+name= input("enter your name: ")
+email= input("enter your email: ")
+
+print(f"Hello {name}, your email is {email}")
+print(f"Thanks {name} for registering, we will send an email to {email}")
+
+number = 20 # ini variabel bil integer
+pi = 3.14 # ini variabel tipe data float
+
+#ini variabel tipe data complexx
+a = 2 + 3j
+
+print(type(number))
+print(type(pi))
+print(type(a))
